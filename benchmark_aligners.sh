0d5915d1e6548c8f7b0e0b7090f354d0f58f0955
Matt Ralston <professional.bio.coder@gmail.com>
2024-12-05 19:03:01 -0500

    Updates to script, includes GSnap, faulty GMap indexing for gsnap (still working on it), SHRiMP, and others.

--- benchmark_aligners.sh
+++ benchmark_aligners.sh
@@ -32,6 +32,7 @@ export PATH=$PATH:$HOME/.pyenv/versions/miniconda3-3.12-24.9.2-0/bin
 ART_ILLUMINA=/ffast/source_files/art_bin_MountRainier/art_illumina
 
 TMPDIR=
+FASTA=
 ORIGINAL_WORKDIR=$(pwd)
 
 # gmapper = SHRiMP
@@ -80,7 +81,7 @@ then
     echo "echo $fasta | grep -E '^*.fasta$'"
     ecode=$?
     echo "Exit code: $ecode"
-    echo "Fasta sequence file does not have a .fasta or .fa file suffix. Possibly gzip/zlib compressed" 1>&2;
+    echo "Fasta sequence file does not have a .fasta or .fa file suffix. Possibly gzip/zlib compressed. Please provide a decompressed fasta file to continue." 1>&2;
     exit 1
 fi
 
@@ -126,6 +127,7 @@ make_project_directory () {
     fasta_abs_dirpath="${full_fasta_filepath%/*}"
     fasta_filename="$(basename $full_fasta_filepath)"
     ln -s $fasta_abs_dirpath/$fasta_filename ./$TMPDIR/
+    FASTA=$fasta_filename
     cd ./$TMPDIR
     echo "Benchmarking aligners in temporary project directory '$(pwd)'" 1>&2;
 }
@@ -146,10 +148,17 @@ print_project_directory_and_exit() {
 }
 
 generate_reads() {
-    $ART_ILLUMINA -ss HS25 -i $fasta -p -l 150 -f 50 -m 200 -s 10 -o $base_filename
+    #$ART_ILLUMINA -ss HS25 -i $fasta -p -qL 30 -l 150 -f 50 -m 200 -s 10 -o $base_filename
     # Additionally combine reads into single fastq file.
-    cat ${base_filename}1.fq ${base_filename}2.fq > ${base_filename}_reads.fq
+    wgsim -N 10000000 $FASTA ${FASTA%.*}_1.fq ${FASTA%.*}_2.fq
+    cat ${FASTA%.*}_1.fq ${FASTA%.*}_2.fq > ${base_filename}_reads.fq
 
+}
+
+
+evaluate_alignments() {
+    samfile=$1
+    wgsim_eval.pl alneval $samfile
 }
 
 
@@ -161,18 +170,27 @@ generate_reads() {
 
 run_bowtie_build() {
     # Generate a bowtie index
-    bowtie-build $fasta $base_filename
+    bowtie-build $FASTA $base_filename
 }
 
 run_bowtie2_build() {
     # Generate a bowtie2 index
-    bowtie2-build $fasta $base_filename
+    bowtie2-build $FASTA $base_filename
 }
 
 run_bbmap_index() {
     # Generate a bbmap index
-    bbmap.sh ref=$fasta
+    bbmap.sh ref=$FASTA
 }
+
+run_gmap_index() {
+    # Generate a GMap/GSnap index
+    gmap_make_ref.sh -D $(pwd) ${FASTA%.*} $FASTA $gtf
+    #make gmapdb
+}
+
+
+
 
 #####################################
 
@@ -181,25 +199,30 @@ run_bbmap_index() {
 #####################################
 run_bowtie() {
     # Generate alignment
-    /usr/bin/time -a -o bowtie_timed.tsv -f "%x\t%e" bowtie -p $cores ${base_filename} -1 ${base_filename}1.fq -2 ${base_filename}2.fq -S ${base_filename}.sam
+    /usr/bin/time -a -o bowtie_timed.tsv -f "%x\t%e" bowtie -p $cores ${base_filename} -1 ${FASTA%.*}_1.fq -2 ${FASTA%.*}_2.fq -S ${FASTA%.*}.bowtie.sam
+    echo "${FASTA%.*}.bowtie.sam"
 }
 
 run_bowtie2() {
     # Run bowtie2
-    /usr/bin/time -a -o bowtie2_timed.tsv -f "%x\t%e" bowtie2 -p $cores -x $base_filename -1 ${base_filename}1.fq -2 ${base_filename}2.fq -S ${base_filename}.sam
+    /usr/bin/time -a -o bowtie2_timed.tsv -f "%x\t%e" bowtie2 -p $cores -x $base_filename -1 ${FASTA%.*}_1.fq -2 ${FASTA%.*}_2.fq -S ${FASTA%.*}.bowtie2.sam
+    echo "${FASTA%.*}.bowtie2.sam"
 }
 
 run_bbmap() {
     # Generate alignment
-    /usr/bin/time -a -o bbmap_timed.tsv -f "%x\t%e" bbmap.sh in=${base_filename}_reads.fq threads=$cores out=${base_filename}.sam 
+    /usr/bin/time -a -o bbmap_timed.tsv -f "%x\t%e" bbmap.sh in=${FASTA%.*}_reads.fq threads=$cores out=${FASTA%.*}.bbmap.sam
+    echo "${FASTA%.*}.bbmap.sam"
 }
 
 run_shrimp() {
-    /usr/bin/time -a -o shrimp_timed.tsv -f "%x\t%e" gmapper -1 ${base_filename}1.fq -2 ${base_filename}2.fq $fasta
+    /usr/bin/time -a -o shrimp_timed.tsv -f "%x\t%e" gmapper -1 ${FASTA%.*}_1.fq -2 ${FASTA%.*}_2.fq $FASTA > ${FASTA%.*}.shrimp.sam
+    echo "${FASTA%.*}.shrimp.sam"
 }
 
 run_gsnap() {
-    /usr/bin/time -a -o gsnap_timed.tsv -f "%x\t%e" gsnap -d $fasta -t $cores --novelsplicing=0 --format=sam ${base_filename}_reads.fq > ${base_filename}.sam
+    /usr/bin/time -a -o gsnap_timed.tsv -f "%x\t%e" gsnap -D $(pwd) -d $FASTA -t $cores --novelsplicing=0 --format=sam ${FASTA%.*}_reads.fq > ${FASTA%.*}.gsnap.sam
+    echo "${FASTA%.*}.gsnap.sam"
 }
 
 
@@ -221,18 +244,26 @@ generate_reads
 run_bowtie_build
 run_bowtie2_build
 run_bbmap_index
+run_gmap_index
+
 
 # Programs
-run_bowtie
-run_bowtie2
-run_bbmap
-#run_shrimp
-#run_gsnap
+bowtie_sam=$(run_bowtie)
+bowtie2_sam=$(run_bowtie2)
+bbmap_sam=$(run_bbmap)
+shrimp_sam=$(run_shrimp)
+gsnap_sam=$(run_gsnap)
+
+# Evaluate alignments
+# evaluate_alignments bowtie_sam
+# evaluate_alignments bowtie2_sam
+# evaluate_alignments bbmap_sam
+# evaluate_alignments shrimp_sam
+# evaluate_alignments gsnap_sam
 
 
 
-
-
+# Print project directory
 print_project_directory_and_exit
 
 
